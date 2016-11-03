ab75c8a6924eaba0434fa820bdf0075e4558de37
chris held <chris.held@zyquest.com>
2016-11-03 16:50:12 -0500

    layout login, some basic flex classes for styling

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
-
+  email: string = "";
+  password: string = "";
   ngOnInit() {
+  }
+  login() {
+
   }
 
 }
